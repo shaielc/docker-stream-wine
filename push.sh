bb8b9ce6dbc58352318b5648351a1c0e3465bbf2
shaielc <shaielshmoel@gmail.com>
2024-03-19 13:13:51 +0200

    QOL

--- push.sh
+++ push.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
+set -x
 docker tag $1-game:latest bouncer582/private:$1-game-latest
 docker tag $1-provider:latest bouncer582/private:$1-provider-latest
 docker tag private-streamer:latest bouncer582/private:streamer-client
 docker push bouncer582/private:streamer-client
 docker push bouncer582/private:$1-game-latest
 docker push bouncer582/private:$1-provider-latest
-
